f7f16a2f84f347da2d7aa3d5008e3e131333d23d
Nikki Lin <nikkiylin@gmail.com>
2020-04-19 18:38:49 -0700

    added projectRouter.js

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const server = express();
 const port = 4000;
 
 const actionRouter = require("./api/actionRouter")
+const projectRouter = require("./api/projectRouter")
 
 server.use(express.json())
 
 server.use("/actions", actionRouter)
+server.use("/projects", projectRouter)
 
 server.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`)
